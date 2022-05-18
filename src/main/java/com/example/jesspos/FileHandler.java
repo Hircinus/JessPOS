538771f5dc6e5197c58c804188382567a78109d1
Jessica <36286032+Hircinus@users.noreply.github.com>
2022-05-17 21:11:16 -0400

    multiscene support working "inventory" tab and return button

---
+++ src/main/java/com/example/jesspos/FileHandler.java
@@ -0,0 +1,5 @@
+package com.example.jesspos;
+
+public class FileHandler {
+
+}
--- src/main/java/com/example/jesspos/HelloApplication.java
+++ src/main/java/com/example/jesspos/HelloApplication.java
@@ -1,11 +1,19 @@
 package com.example.jesspos;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventTarget;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Tab;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import org.kordamp.bootstrapfx.BootstrapFX;
 import org.kordamp.bootstrapfx.scene.layout.Panel;
@@ -13,32 +21,77 @@ import org.kordamp.bootstrapfx.scene.layout.Panel;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-        /*
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+    GridPane content = new GridPane();
+    public Scene scene = new Scene(content);
+    @Override public void start(Stage stage) throws Exception {
+        home();
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setScene(scene);
+        stage.setTitle("JessPOS");
         stage.show();
-
-         */
-        Panel panel = new Panel("This is the title");
-        panel.getStyleClass().add("panel-primary");                            //(2)
-        BorderPane content = new BorderPane();
-        content.setPadding(new Insets(20));
-        Button button = new Button("Hello BootstrapFX");
-        button.getStyleClass().setAll("btn","btn-danger");                     //(2)
-        content.setCenter(button);
-        panel.setBody(content);
-
-        Scene scene = new Scene(panel);
-        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());       //(3)
-
-        stage.setTitle("BootstrapFX");
-        stage.setScene(scene);
-        stage.sizeToScene();
-        stage.show();
+    }
+    public void home() {
+        menuBtn test = new menuBtn("Click me!", "btn-success");
+        test.setOnAction(new InventoryView());
+        menuBtn test2 = new menuBtn("Click me!", "btn-primary");
+        menuBtn test3 = new menuBtn("Click me!", "btn-warning");
+        menuBtn test4 = new menuBtn("Click me!", "btn-danger");
+        content.setAlignment(Pos.CENTER);
+        content.add(test, 0, 0);
+        content.add(test2, 1, 0);
+        content.add(test3, 0, 1);
+        content.add(test4, 1, 1);
+        for (int i = 0 ; i < 2 ; i++) {
+            ColumnConstraints cc = new ColumnConstraints();
+            cc.setPercentWidth(100.0/2.0);
+            cc.setHgrow(Priority.ALWAYS);
+            content.getColumnConstraints().add(cc);
+        }
+        for (int i = 0 ; i < 2 ; i++) {
+            RowConstraints rc = new RowConstraints();
+            rc.setPercentHeight(100.0/2.0);
+            rc.setVgrow(Priority.ALWAYS);
+            content.getRowConstraints().add(rc);
+        }
+        RowConstraints rc = new RowConstraints();
+        rc.setVgrow(Priority.ALWAYS);
+        content.getRowConstraints().add(rc);
+    }
+    public void sceneClear() {
+        content.getChildren().clear();
+        content.getColumnConstraints().clear();
+        content.getRowConstraints().clear();
+    }
+    class HomeView implements EventHandler<ActionEvent>
+    {
+        @Override
+        public void handle(ActionEvent event)
+        {
+            sceneClear();
+            home();
+        }
+    }
+    class InventoryView implements EventHandler<ActionEvent>
+    {
+        @Override
+        public void handle(ActionEvent event)
+        {
+            ListView inventory = new ListView();
+            sceneClear();
+            content.add(inventory,0,0);
+            menuBtn back = new menuBtn("Return", "btn-warning");
+            back.setOnAction(new HomeView());
+            content.add(back,1,0);
+            inventory.getItems().addAll("hello","this","is","a","test");
+        }
+    }
+    private class menuBtn extends Button {
+        public menuBtn(String text, String btnType) {
+            super(text);
+            this.getStyleClass().addAll("btn", btnType);
+            this.setMinHeight(150);
+            this.setMinWidth(150);
+        }
     }
 
     public static void main(String[] args) {
